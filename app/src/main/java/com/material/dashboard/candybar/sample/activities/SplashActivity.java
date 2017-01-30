b7dc04005be3450607c7fb9a9bc423fa1733e35b
Dani Mahardhika <danimahardhika112@gmail.com>
2017-01-30 19:53:07 +0700

    Update follow release 1.6.0

--- app/src/main/java/com/material/dashboard/candybar/sample/activities/SplashActivity.java
+++ app/src/main/java/com/material/dashboard/candybar/sample/activities/SplashActivity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.dm.material.dashboard.candybar.activities.CandyBarSplashActivity;
-import com.material.dashboard.candybar.sample.R;
 
 public class SplashActivity extends CandyBarSplashActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        int duration = getResources().getInteger(R.integer.splash_screen_duration);
-        initSplashActivity(savedInstanceState, MainActivity.class, duration);
+        initSplashActivity(savedInstanceState, MainActivity.class);
     }
 }
