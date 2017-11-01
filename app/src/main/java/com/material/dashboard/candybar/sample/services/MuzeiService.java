6ec783a0c2f8ae20c445bf3f1e3cfcaa3dffedbb
Dani Mahardhika <danimahardhika112@gmail.com>
2017-11-01 10:54:04 +0700

    Update MuzeiService.java

--- app/src/main/java/com/material/dashboard/candybar/sample/services/MuzeiService.java
+++ app/src/main/java/com/material/dashboard/candybar/sample/services/MuzeiService.java
@@ -1,9 +1,6 @@
 package com.material.dashboard.candybar.sample.services;
 
-import android.content.Intent;
-
 import com.dm.material.dashboard.candybar.services.CandyBarMuzeiService;
-import com.material.dashboard.candybar.sample.R;
 
 public class MuzeiService extends CandyBarMuzeiService {
 
@@ -12,22 +9,4 @@ public class MuzeiService extends CandyBarMuzeiService {
     public MuzeiService() {
         super(SOURCE_NAME);
     }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        startCommand(intent, flags, startId);
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onCreate() {
-        initMuzeiService();
-    }
-
-    @Override
-    protected void onTryUpdate(int reason) throws RetryException {
-        String wallpaperUrl = getResources().getString(R.string.wallpaper_json);
-        tryUpdate(wallpaperUrl);
-    }
-
 }
