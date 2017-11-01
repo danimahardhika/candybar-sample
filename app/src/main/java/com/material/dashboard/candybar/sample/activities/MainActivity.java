8b96c39e5fd188298cd200eb7752e46b813d0fb7
Dani Mahardhika <danimahardhika112@gmail.com>
2017-11-01 10:51:04 +0700

    Update MainActivity.java

--- app/src/main/java/com/material/dashboard/candybar/sample/activities/MainActivity.java
+++ app/src/main/java/com/material/dashboard/candybar/sample/activities/MainActivity.java
@@ -1,23 +1,21 @@
 package com.material.dashboard.candybar.sample.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 
 import com.dm.material.dashboard.candybar.activities.CandyBarMainActivity;
-import com.dm.material.dashboard.candybar.helpers.InAppBillingHelper;
+import com.dm.material.dashboard.candybar.activities.configurations.ActivityConfiguration;
 import com.material.dashboard.candybar.sample.licenses.License;
 
 public class MainActivity extends CandyBarMainActivity {
 
+    @NonNull
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        initMainActivity(savedInstanceState,
-                new InAppBillingHelper.Property(
-                        License.isLicenseCheckerEnabled(),
-                        License.getRandomString(),
-                        License.getLicenseKey(),
-                        License.getDonationProductsId(),
-                        License.getPremiumRequestProductsId(),
-                        License.getPremiumRequestProductsCount()));
+    public ActivityConfiguration onInit() {
+        return new ActivityConfiguration()
+                .setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
+                .setLicenseKey(License.getLicenseKey())
+                .setRandomString(License.getRandomString())
+                .setDonationProductsId(License.getDonationProductsId())
+                .setPremiumRequestProducts(License.getDonationProductsId(), License.getPremiumRequestProductsCount());
     }
 }
